fbc0d84004fe0f99f5ec7826d0f6ae7324a29e97
eric <ericsilva328@gmail.com>
2020-09-24 19:52:27 -0300

    Inicio do codlab 1.2

---
+++ app/src/main/java/com/aprendizadodo/kotlinfundamentals/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aprendizadodo.kotlinfundamentals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
